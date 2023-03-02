ae71223b21788cb4e3f5c00e10b2c026376568f9
DanyT <86497591+tomyjuly@users.noreply.github.com>
2023-03-02 10:03:12 -0300

    initial commit

---
+++ js/function.js
@@ -0,0 +1,25 @@
+
+    function colorrojo() 
+    {
+        document.getElementById('titulo').style.color = 'red';
+    }
+
+    function colornegro() 
+    {
+        document.getElementById('titulo').style.color = 'black';
+    }
+
+
+    function agrandarfoto() 
+    {
+        document.getElementById('cvfoto').style.height= '200px';
+        document.getElementById('cvfoto').style.width= '200px';
+    }
+
+    function achicarfoto() 
+    {
+       document.getElementById('cvfoto').style.height= '100px';
+       document.getElementById('cvfoto').style.width= '100px';
+    }
+
+
